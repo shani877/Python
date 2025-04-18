f79d402ac187b8b932129550b81bbf2a446233b5
Shanu Jagtap <shanujagtap6266@gmail.com>
2025-04-18 18:06:35 +0530

    Create calculation.py

---
+++ calculation.py
@@ -0,0 +1,17 @@
+num1 = int(input("enter first number: "))
+num2 = int(input("enter second number: "))
+
+print(type(num1))
+print(type(num2))
+
+sum_of_num = num1 + num2
+print("Sum of 2 numbers: ", sum_of_num)
+
+diff_of_num = num1 - num2
+print("Diff of 2 numbers: ", diff_of_num)
+
+prod_of_num = num1 * num2
+print("Multiply of 2 numbers: ", prod_of_num)
+
+div_of_num = num1 / num2
+print("Division of 2 numbers: ", div_of_num)
