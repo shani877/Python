fdbff7cdeb6823b0e3c03abb6e2a18476bd71ad7
Shanu Jagtap <shanujagtap6266@gmail.com>
2025-04-18 18:04:52 +0530

    Create conditionals.py

---
+++ conditionals.py
@@ -0,0 +1,7 @@
+day_of_week = input("Enter the day of week: ").lower()
+print(day_of_week)
+
+if day_of_week == "saturday" or day_of_week == "monday":
+  print("I will learn python")
+else:
+  print("I will learn terraform")
